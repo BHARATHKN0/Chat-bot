952b7fbe97d5271609281856799e9834ceaf5095
BHARATH K N <93982711+BHARATHKN0@users.noreply.github.com>
2023-10-10 12:38:21 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 export const fetchResponse =  async(chat) => {
     try {
         // after depoloyment you should change the fetch URL below
-        const response = await fetch('https://chatbot-server-green.vercel.app/', { 
+        const response = await fetch('Web link of the deployed server', { 
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
